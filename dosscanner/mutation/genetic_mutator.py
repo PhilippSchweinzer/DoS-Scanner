fb13528000d78cc8b1d729aa0da26ac099c33cb1
PhilippSchweinzer <schweinzerphilipp@gmail.com>
2024-04-16 10:19:50 +0200

    improved genetic mutator

--- dosscanner/mutation/genetic_mutator.py
+++ dosscanner/mutation/genetic_mutator.py
@@ -37,7 +37,8 @@ class GeneticMutator(Mutator):
                 )
             )
 
-        for evolution in range(self.max_evolutions):
+        # Iterate over all evolutions
+        for _ in range(self.max_evolutions):
             # Reset feedback data
             self.feedback_data = []
 
@@ -60,16 +61,24 @@ class GeneticMutator(Mutator):
         self.feedback_data.append(endpoint)
 
     def _select_parents(
-        self, endpoints: list[GeneticEndpoint]
+        self, population: list[GeneticEndpoint]
     ) -> list[GeneticEndpoint]:
+        """Select viable parents from the population
+
+        Args:
+            population (list[GeneticEndpoint]): Population of the current evolution stage
+
+        Returns:
+            list[GeneticEndpoint]: Selected parents viable for further evolution
+        """
 
         # Sort by greatest improvement in response time compared to the parent
         sorted_by_measurement_diff = sorted(
-            endpoints, key=lambda e: e.measurement - e.parent.measurement, reverse=True
+            population, key=lambda e: e.measurement - e.parent.measurement, reverse=True
         )
 
         # Calculate the bias weights for randomly choosing from the list
-        weights = [1 / (i + 1) for i in range(self.population_size)]
+        weights = [1 / (i + 0.5) for i in range(self.population_size)]
         # Using biased randomness favoring parents with greater time improvement to choose a new population
         parents = random.choices(
             sorted_by_measurement_diff, weights, k=self.population_size
@@ -77,6 +86,14 @@ class GeneticMutator(Mutator):
         return parents
 
     def _mutate(self, endpoint: GeneticEndpoint) -> GeneticEndpoint:
+        """Mutate the endpoint by chaning properties of its parameters
+
+        Args:
+            endpoint (GeneticEndpoint): Endpoint from which the mutation is generated
+
+        Returns:
+            GeneticEndpoint: Mutated endpoint
+        """
         params = endpoint.get_url_params()
 
         if len(params) == 0:
@@ -86,7 +103,8 @@ class GeneticMutator(Mutator):
 
         mutations = [
             Mutations.add_digit,
-            Mutations.add_character,
+            Mutations.add_lowercase_character,
+            Mutations.add_uppercase_character,
             Mutations.add_special_character,
         ]
         params[chosen_param] = random.choice(mutations)(params[chosen_param])
@@ -108,8 +126,13 @@ class Mutations:
         return param + str(random.randint(0, 9))
 
     @staticmethod
-    def add_character(param: str):
+    def add_lowercase_character(param: str):
         return param + random.choice(string.ascii_lowercase)
 
+    @staticmethod
+    def add_uppercase_character(param: str):
+        return param + random.choice(string.ascii_uppercase)
+
+    @staticmethod
     def add_special_character(param: str):
         return param + random.choice(".,:;-+_#*~?!/\\<>")
