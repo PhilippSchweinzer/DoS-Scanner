be89a38e3d89ef8c9a1b8adc35a18cc7a907ae37
PhilippSchweinzer <schweinzerphilipp@gmail.com>
2024-04-17 22:29:51 +0200

    added crawler requests to central Requestor

--- dosscanner/request.py
+++ dosscanner/request.py
@@ -1,4 +1,6 @@
 from concurrent.futures import ThreadPoolExecutor
+from dataclasses import dataclass
+from typing import Callable
 
 import requests
 from ratelimit import limits, sleep_and_retry
@@ -10,6 +12,10 @@ from dosscanner.statistics import arithmetic_mean
 
 class Requestor:
 
+    headers: dict = {}
+    certificate_validation: bool = True
+    proxies: dict = {}
+
     queue: list[Endpoint] = []
     rate_limit: int = 100
 
@@ -18,35 +24,74 @@ class Requestor:
         Requestor.queue.append(endpoint)
 
     @staticmethod
-    def evaluate() -> list[int]:
+    def evaluate_response_body() -> list["ResponseData"]:
+
+        @sleep_and_retry
+        @limits(calls=Requestor.rate_limit, period=1)
+        def get_response_body(url: str) -> str:
+            print(url)
+            try:
+                resp = requests.get(
+                    url,
+                    headers=Requestor.headers,
+                    timeout=30,
+                    proxies=Requestor.proxies,
+                    verify=Requestor.certificate_validation,
+                )
+            except Exception as exc:
+                return ResponseData(body="", url="")
+
+            return ResponseData(body=resp.text, url=resp.url)
+
+        return Requestor._evaluate(get_response_body)
+
+    @staticmethod
+    def evaluate_response_time() -> list[int]:
 
         @sleep_and_retry
         @limits(calls=Requestor.rate_limit, period=1)
         def get_response_time(url: str) -> int:
             try:
-                resp = requests.get(url, timeout=60)
+                resp = requests.get(
+                    url,
+                    headers=Requestor.headers,
+                    timeout=60,
+                    proxies=Requestor.proxies,
+                    verify=Requestor.certificate_validation,
+                )
             except Timeout:
                 return 60 * 1_000_000
-            except Exception:
+            except Exception as exc:
                 return -1
 
             return resp.elapsed.microseconds
 
-        def measure_endpoint(endpoint: Endpoint) -> int:
+        def measure_endpoint(url: str) -> int:
             # Calculate arithmetic mean from multiple responses to get more accurate result
-            return arithmetic_mean([get_response_time(endpoint.url) for _ in range(5)])
+            return arithmetic_mean([get_response_time(url) for _ in range(5)])
 
+        return Requestor._evaluate(measure_endpoint)
+
+    @staticmethod
+    def _evaluate(func: Callable) -> list:
         # If the queue only consists of a small amount of items
         # Skip the overhead of creating a thread pool and just evaluate
         # them one by one
         if len(Requestor.queue) <= 3:
-            results = [measure_endpoint(endpoint) for endpoint in Requestor.queue]
+            results = [func(endpoint.url) for endpoint in Requestor.queue]
         else:
             # Evaluate all items from queue
             with ThreadPoolExecutor(max_workers=10) as executor:
-                results = list(executor.map(measure_endpoint, Requestor.queue))
+                tasks = [endpoint.url for endpoint in Requestor.queue]
+                results = list(executor.map(func, tasks))
 
         # Clear queue after processing it
         Requestor.queue.clear()
 
         return results
+
+
+@dataclass
+class ResponseData:
+    body: str
+    url: str
