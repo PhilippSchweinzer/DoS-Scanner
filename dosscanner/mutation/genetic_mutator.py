016fbfb9f4b7e519e0c2690b28cdb73d7e4241e6
PhilippSchweinzer <schweinzerphilipp@gmail.com>
2024-04-17 22:29:28 +0200

    improved genetic mutator

--- dosscanner/mutation/genetic_mutator.py
+++ dosscanner/mutation/genetic_mutator.py
@@ -1,7 +1,7 @@
-import copy
 import random
 import string
 from collections.abc import Iterator
+from copy import copy, deepcopy
 
 from typing_extensions import override
 
@@ -18,50 +18,114 @@ class GeneticMutator(Mutator):
 
     @override
     def next(self, item: Endpoint) -> Iterator[tuple[Endpoint, bool]]:
-        # Reset feedback data
-        self.feedback_data.clear()
+        """Uses the item and mutates it using a genetic algorithm.
 
-        # Yield the original item to measure it and create a baseline reading
-        yield item, True
+        Args:
+            item (Endpoint): Endpoint which is mutated
 
-        # Create initial population
-        population = []
-        initial_measurement = self.feedback_data[0].measurement
-        for _ in range(self.population_size):
-            population.append(
-                GeneticEndpoint(
-                    url=item.url,
-                    http_method=item.http_method,
-                    measurement=initial_measurement,
-                    parent=None,
-                )
-            )
-
-        # Iterate over all evolutions
-        for _ in range(self.max_evolutions):
+        Yields:
+            Iterator[tuple[Endpoint, bool]]: All mutations generated
+        """
+        for param in item.get_url_params():
             # Reset feedback data
             self.feedback_data.clear()
 
-            # Mutate population
-            for pop in population[:-1]:
-                yield self._mutate(pop), False
-            yield self._mutate(population[-1]), True
+            # Yield the original item to measure it and create a baseline reading
+            yield item, True
+            initial_measurement = self.feedback_data[0].measurement
 
-            # Evaluate fitness
-            # This step is done in the scanner implementation
+            # Create initial population
+            population = [
+                GeneticEndpoint(
+                    url=item.url,
+                    http_method=item.http_method,
+                    measurement=None,
+                    mutated_param_key=param,
+                )
+            ] * self.population_size
 
-            # Create new generation by selecting viable parents
-            population = self._select_parents(self.feedback_data)
+            # Iterate over all evolutions
+            for _ in range(self.max_evolutions):
+                # Reset feedback data
+                self.feedback_data.clear()
+
+                # Create a deepcopy of the population to initiate a new step
+                population = [deepcopy(pop) for pop in population]
+
+                # Mutate population
+                for pop in population:
+                    if random.random() < 0.3:
+                        self._mutate(pop)
+                        print(pop)
+
+                # Evaluate fitness
+                for pop in population[:-1]:
+                    yield pop, False
+                yield population[-1], True
+
+                # Select viable parent from population
+                parent_population = self._select_parents(
+                    self.feedback_data, initial_measurement
+                )
+
+                # Execute crossover to fill population with ancestors from
+                # the viable parents
+                population = self._crossover(parent_population)
 
     @override
     def feedback(self, endpoint: GeneticEndpoint, measurement: int):
+        """Delivers feedback to the Mutator which can be used by the next(...) call to
+           further improve mutation.
+
+        Args:
+            endpoint (GeneticEndpoint): GeneticEndpoint of which the feedback is given
+            measurement (int): Feedback value representing the response time of the endpoint
+        """
         endpoint.measurement = measurement
         self.feedback_data.append(endpoint)
 
-    def _select_parents(
-        self, population: list[GeneticEndpoint]
+    # One point crossover
+    def _crossover(
+        self, parent_population: list[GeneticEndpoint]
     ) -> list[GeneticEndpoint]:
-        """Select viable parents from the population
+        """Represents the crossover phase in the genetic algorithm.
+           Takes a list of parents and fills the population by crossing their properties to
+           create offspring until the population has reached its maximum.
+
+        Args:
+            parent_population (list[GeneticEndpoint]): Parents from which offspring is created
+
+        Returns:
+            list[GeneticEndpoint]: New generation of population
+        """
+        population = parent_population.copy()
+
+        while len(population) != self.population_size:
+            # Choose two parents
+            parent1, parent2 = random.sample(parent_population, k=2)
+            param1 = parent1.get_mutated_param_value()
+            param2 = parent2.get_mutated_param_value()
+            min_len = min(len(param1), len(param2))
+            crossover_point = random.randint(0, min_len)
+            offspring = GeneticEndpoint(
+                url=parent1.url,
+                http_method=parent1.http_method,
+                measurement=None,
+                mutated_param_key=parent1.mutated_param_key,
+            )
+            crossover_value = param1[:crossover_point] + param2[crossover_point:]
+            offspring.set_mutated_param_value(crossover_value)
+            population.append(offspring)
+
+        return population
+
+    def _select_parents(
+        self, population: list[GeneticEndpoint], initial_measurement: int
+    ) -> list[GeneticEndpoint]:
+        """Represents the natural selection phase in the genetic algorithm.
+           Select viable parents from the population using a biased randomness to
+           favor endpoints with the greatest improvement in response time with respect
+           to its original ancestor.
 
         Args:
             population (list[GeneticEndpoint]): Population of the current evolution stage
@@ -71,19 +135,20 @@ class GeneticMutator(Mutator):
         """
         # Sort by greatest improvement in response time compared to the parent
         sorted_by_measurement_diff = sorted(
-            population, key=lambda e: e.measurement - e.parent.measurement, reverse=True
+            population, key=lambda e: e.measurement - initial_measurement, reverse=True
         )
 
         # Calculate the bias weights for randomly choosing from the list
         weights = [1 / (i + 0.5) for i in range(self.population_size)]
         # Using biased randomness favoring parents with greater time improvement to choose a new population
         parents = random.choices(
-            sorted_by_measurement_diff, weights, k=self.population_size
+            sorted_by_measurement_diff, weights, k=self.population_size // 2
         )
         return parents
 
     def _mutate(self, endpoint: GeneticEndpoint) -> GeneticEndpoint:
-        """Mutate the endpoint by chaning properties of its parameters
+        """Represents the mutation phase in the genetic algorithm
+           Mutate the endpoint by changing its properties
 
         Args:
             endpoint (GeneticEndpoint): Endpoint from which the mutation is generated
@@ -91,29 +156,22 @@ class GeneticMutator(Mutator):
         Returns:
             GeneticEndpoint: Mutated endpoint
         """
-        params = endpoint.get_url_params()
-
-        if len(params) == 0:
-            return endpoint
-
-        chosen_param = random.choice(list(params.keys()))
-
         mutations = [
             Mutations.add_digit,
+            Mutations.negate,
             Mutations.add_lowercase_character,
             Mutations.add_uppercase_character,
             Mutations.add_special_character,
+            # Mutations.add_copy,
+            Mutations.remove_last_character,
         ]
-        params[chosen_param] = random.choice(mutations)(params[chosen_param])
-
-        mutated_endpoint = GeneticEndpoint(
-            url=endpoint.url,
-            http_method=endpoint.http_method,
-            measurement=None,
-            parent=endpoint,
+        params = endpoint.get_url_params()
+        params[endpoint.mutated_param_key] = random.choice(mutations)(
+            params[endpoint.mutated_param_key]
         )
-        mutated_endpoint.set_url_params(params)
-        return mutated_endpoint
+        endpoint.set_url_params(params)
+
+        return endpoint
 
 
 class Mutations:
@@ -123,13 +181,27 @@ class Mutations:
         return param + str(random.randint(0, 9))
 
     @staticmethod
-    def add_lowercase_character(param: str):
+    def negate(param: str) -> str:
+        return "-" + param
+
+    @staticmethod
+    def add_lowercase_character(param: str) -> str:
         return param + random.choice(string.ascii_lowercase)
 
     @staticmethod
-    def add_uppercase_character(param: str):
+    def add_uppercase_character(param: str) -> str:
         return param + random.choice(string.ascii_uppercase)
 
     @staticmethod
-    def add_special_character(param: str):
+    def add_special_character(param: str) -> str:
         return param + random.choice(".,:;-+_#*~?!/\\<>")
+
+    @staticmethod
+    def add_copy(param: str) -> str:
+        return param + param
+
+    @staticmethod
+    def remove_last_character(param: str) -> str:
+        if len(param) > 1:
+            return param[:-1]
+        return param
